61e4e98a67fdf5a5855c416724510ecc0463b7ed
Orly Dahan <orly.dahan@correo.unimet.edu.ve>
2019-10-29 23:10:42 -0400

    Progamas descargados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { TabsModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    TabsModule.forRoot(),
+    TooltipModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
