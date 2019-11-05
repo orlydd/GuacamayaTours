c3172d6de504b17d178d5cafb49765048e9cd6c1
Amanda Gonzalez <amanda.gonzalez@correo.unimet.edu.ve>
2019-11-04 20:36:10 -0400

    Firebase y Firestore implementados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { TabsModule } from 'ngx-bootstrap';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     TabsModule.forRoot(),
     TooltipModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBmol8Sqc2MWsQJNANlNf7Vu-QPI6fG1fQ",
+    authDomain: "guacamayatours-cbc79.firebaseapp.com",
+    databaseURL: "https://guacamayatours-cbc79.firebaseio.com",
+    projectId: "guacamayatours-cbc79",
+    storageBucket: "guacamayatours-cbc79.appspot.com",
+    messagingSenderId: "1010087553466",
+    appId: "1:1010087553466:web:03f5ce2248c6e9bb045583",
+    measurementId: "G-H0VTVY6E6S"
+  }
 };
 
 /*
