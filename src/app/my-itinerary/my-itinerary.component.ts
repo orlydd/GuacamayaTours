b320137d00474402f3c88e1f70f52101f7a6f2c8
Orly Dahan <orly.dahan@correo.unimet.edu.ve>
2019-11-20 21:17:33 -0400

    Master

--- src/app/my-itinerary/my-itinerary.component.ts
+++ src/app/my-itinerary/my-itinerary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ItineraryService} from '../services/itinerary/itinerary.service'
 
 @Component({
   selector: 'app-my-itinerary',
@@ -7,12 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyItineraryComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() { 
+  }
 
   ngOnInit() {
   }
 
-  check(){
+  checkCode(code:string){
     
   }
 
