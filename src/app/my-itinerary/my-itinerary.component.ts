736d1cecb83b683e115907799cdd19431fa5e42d
Orly Dahan <orly.dahan@correo.unimet.edu.ve>
2019-11-28 18:39:58 -0400

    seeItinerary done

--- src/app/my-itinerary/my-itinerary.component.ts
+++ src/app/my-itinerary/my-itinerary.component.ts
@@ -1,8 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
 import {ItineraryService} from '../services/itinerary/itinerary.service'
 import { Itinerary } from '../models/itinerary';
-import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestoreCollection, AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { NgForm } from '@angular/forms';
+export interface Itinerary { arrivalDate:string, departureDate:string, destination:string,hotel:string };
 
 @Component({
   selector: 'app-my-itinerary',
@@ -11,22 +13,15 @@ import { NgForm } from '@angular/forms';
 })
 export class MyItineraryComponent implements OnInit {
 
-  itinerary : Itinerary[];
+  itinerary : Itinerary;
   itineraryCollection: AngularFirestoreCollection<Itinerary>;
   codeEntered: string;
-  itineraryDoc: {arrivalDate:string, departureDate:string, destination:string, hotel:string};
+  itineraryDoc: AngularFirestoreDocument<Itinerary>;
   show: boolean;
 
 
   constructor( private db: AngularFirestore, private itineraryService: ItineraryService) { 
-    this.itineraryCollection = this.db.collection('Itinerary');
-    
-   
-    this.itineraryService.getItinerary().subscribe(
-     itinerary =>{
-       this.itinerary = itinerary;
-     }
-   )
+ 
   }
 
 
@@ -36,26 +31,14 @@ export class MyItineraryComponent implements OnInit {
 
 
   onSubmit(form: NgForm){
-    console.log(this.itineraryDoc);
     this.checkCode(form);
-    console.log(this.itineraryDoc.arrivalDate);
   }
 
   checkCode(form: NgForm) {
     this.show= true;
     let codeEntered = form.value.code;
-    let code = this.db.collection('Itinerary', ref=>ref.where('itineraryCode', '==', codeEntered));
-    code.get().toPromise().then(querySnapshot=>{
-      querySnapshot.forEach(doc=>{
-        console.log(querySnapshot.docs);
-        let itineraryDoc = {...doc.data()};
-        return itineraryDoc;
-      });
-      
-    this.itineraryDoc = querySnapshot;
-    }).catch(e=>{
-      console.log(e);
-    });
-  }
+    this.db.doc<Itinerary>(`Itinerary/${codeEntered}`).get().toPromise().then(snapshot => this.itinerary = snapshot.data() as Itinerary);
+}
+
 
 }
