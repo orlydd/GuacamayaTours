1e81f14d683353ec9d5493dca505c5b9eca54b28
amandalug17 <amandalug17@gmail.com>
2019-11-28 08:34:28 -0400

    hotels models

--- src/app/models/Hotels.model.ts
+++ src/app/models/Hotels.model.ts
@@ -1,43 +1,28 @@
 export class Hotels{
-    id: String;
-    name: String;
-    stars: number;
+    key: string;
+    hotelsId: string;
+    availability: number;
+    city: string;
+    category: string;
+    description: string;
     latitude: number;
     longitude: number;
-    direction: String;
-    state: String;
-    photos:Photo;
-    city: String;
-    pricePerPerson: number;
-    services: Services;
-    pricePerNight: number;
-    roomType: Room;
+    name: string;
+    photo1: string;
+    photo2: string;
+    photo3: string;
+    photos: string;
+    popularity: number;
+    price: number;
+    stars: number;
+    state: string;
+    services: boolean[];
+    rooms: Room[];
 }
 
 export class Room{
     name: string;
-    photo: Photo;
-    maxPeople: number;
-    comodities: Comodities;
-}
-export class Comodities{
-    comodity1: string;
-    comodity2: string;
-    comodity3: string;
-    comodity4: string;
-    comodity5: string;
-}
-export class Photo{
-    photo1: string;
-    photo2: string;
-    photo3: string;
-    photo4: string;
-    photo5: string;
-}
-export class Services{
-    service1: string;
-    service2: string;
-    service3: string;
-    service4: string;
-    service5: string;
+    photo:string;
+    price: number;
+    
 }
